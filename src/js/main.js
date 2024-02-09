47f0ddfd62cbc47d9ccc95a7071a9dddbdb49534
Bubbl33s <jairo.lf.8@gmail.com>
2024-02-09 13:01:04 -0500

    b

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { auth } from "../app/firebase.js";
 
-import '../app/app/signup_form.js';
-import '../app/app/signin_form.js';
-import '../app/app/logout.js';
+import '../app/signup_form.js';
+import '../app/signin_form.js';
+import '../app/logout.js';
 import { loginCheck } from '../app/app/login_check.js';
 
 onAuthStateChanged(auth, async (user) => {
